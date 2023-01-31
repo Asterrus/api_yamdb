e3d3f938615b807a5d7a4fa6467a75841c20b3b8
Asterrus <gastursolar@gmail.com>
2023-01-31 18:26:20 +0500

    Добавил класс-вью для отправки кода подтверждения по email, сериализатор для него и добавил путь в файле urls.

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+
+
+class SignUpSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True, max_length=254)
+    username = serializers.RegexField(
+        required=True, max_length=150, regex=r'^[\w.@+-]+$')
+
+    def validate_username(self, value):
+        if value == 'me':
+            raise serializers.ValidationError('me - not allowed username')
+        return value
