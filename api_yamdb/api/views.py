34d6f080566792ca3e56f499caba8e7ab0fbd48d
artyom-vah <114044516+artyom-vah@users.noreply.github.com>
2023-01-31 16:19:06 +0500

    коммит

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# class CategoryViewSet(CreateListDestroyViewSet):
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+    
+# class GenresViewSet(CreateListDestroyViewSet):
+#     queryset = Genre.objects.all()
+#     serializer_class = GenreSerializer
